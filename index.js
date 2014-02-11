58e19652a39295828779b08fa77093628fe11588
Blixt <blixt@spotify.com>
2014-02-11 11:06:42 -0500

    Support including with <script> tag

--- index.js
+++ index.js
@@ -1,71 +1,79 @@
-function receiveCallsFromOwner(functions) {
-  self.addEventListener('message', function (e) {
-    var message = e.data;
+;(function (commonjs) {
+  function receiveCallsFromOwner(functions) {
+    self.addEventListener('message', function (e) {
+      var message = e.data;
 
-    if (message.call) {
-      var requestId = message.requestId;
+      if (message.call) {
+        var requestId = message.requestId;
 
-      // Find the function to be called.
-      var fn = functions[message.call];
-      if (!fn) {
-        self.postMessage({
-          callResponse: requestId,
-          arguments: ['That function does not exist']
+        // Find the function to be called.
+        var fn = functions[message.call];
+        if (!fn) {
+          self.postMessage({
+            callResponse: requestId,
+            arguments: ['That function does not exist']
+          });
+          return;
+        }
+
+        var args = message.arguments || [];
+        args.push(function () {
+          var args = Array.prototype.slice.call(arguments);
+          self.postMessage({callResponse: requestId, arguments: args});
         });
-        return;
+        fn.apply(functions, args);
       }
+    });
+  }
 
-      var args = message.arguments || [];
-      args.push(function () {
-        var args = Array.prototype.slice.call(arguments);
-        self.postMessage({callResponse: requestId, arguments: args});
-      });
-      fn.apply(functions, args);
-    }
-  });
-}
+  function sendCallsToWorker(worker) {
+    var callbacks = {};
 
-function sendCallsToWorker(worker) {
-  var callbacks = {};
+    worker.addEventListener('message', function (e) {
+      var message = e.data;
 
-  worker.addEventListener('message', function (e) {
-    var message = e.data;
+      if (message.callResponse) {
+        var requestId = message.callResponse;
+        callbacks[requestId].apply(null, message.arguments);
+        delete callbacks[requestId];
+      }
+    });
 
-    if (message.callResponse) {
-      var requestId = message.callResponse;
-      callbacks[requestId].apply(null, message.arguments);
-      delete callbacks[requestId];
-    }
-  });
+    var nextRequestId = 1;
 
-  var nextRequestId = 1;
-
-  var getHandler = function (_, name) {
-    return function () {
-      var id = nextRequestId++,
-          args = Array.prototype.slice.call(arguments);
-      callbacks[id] = args.pop();
-      worker.postMessage({requestId: id, call: name, arguments: args});
+    var getHandler = function (_, name) {
+      return function () {
+        var id = nextRequestId++,
+            args = Array.prototype.slice.call(arguments);
+        callbacks[id] = args.pop();
+        worker.postMessage({requestId: id, call: name, arguments: args});
+      };
     };
-  };
 
-  if (Proxy.create) {
-    return Proxy.create({get: getHandler});
-  } else if (Proxy) {
-    return new Proxy({}, {get: getHandler});
-  } else {
-    throw new Error('Proxy support required');
+    if (Proxy.create) {
+      return Proxy.create({get: getHandler});
+    } else if (Proxy) {
+      return new Proxy({}, {get: getHandler});
+    } else {
+      throw new Error('Proxy support required');
+    }
   }
-}
 
-/**
- * Call this function with either a Worker instance or a map of functions that
- * can be called inside the worker.
- */
-module.exports = function (workerOrFunctions) {
-  if (this.Worker && (workerOrFunctions instanceof Worker)) {
-    return sendCallsToWorker(workerOrFunctions);
-  } else {
-    receiveCallsFromOwner(workerOrFunctions);
+  /**
+   * Call this function with either a Worker instance or a map of functions that
+   * can be called inside the worker.
+   */
+  function createWorkerProxy(workerOrFunctions) {
+    if (this.Worker && (workerOrFunctions instanceof Worker)) {
+      return sendCallsToWorker(workerOrFunctions);
+    } else {
+      receiveCallsFromOwner(workerOrFunctions);
+    }
   }
-};
+
+  if (commonjs) {
+    module.exports = createWorkerProxy;
+  } else {
+    (this.window || this.self).createWorkerProxy = createWorkerProxy;
+  }
+})(this.module && typeof module.exports == 'object');
